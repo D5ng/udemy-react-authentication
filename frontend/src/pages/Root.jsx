56c93f92e2daac95c1ab5f21a77ab2df33f3a8bc
d5ng <dong5ffice@gmail.com>
2023-07-15 19:04:58 +0900

    auto logout

--- frontend/src/pages/Root.jsx
+++ frontend/src/pages/Root.jsx
@@ -1,9 +1,21 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import { Outlet, useLoaderData, useNavigation, useSubmit } from "react-router-dom"
 
-import MainNavigation from '../components/MainNavigation';
+import MainNavigation from "../components/MainNavigation"
+import { useEffect } from "react"
 
 function RootLayout() {
+  const token = useLoaderData()
+  const submit = useSubmit()
   // const navigation = useNavigation();
+  useEffect(() => {
+    if (!token) return
+
+    const timer = setTimeout(() => {
+      submit(null, { method: "POST", action: "/logout" })
+    }, 60 * 60 * 1000)
+
+    return () => clearTimeout(timer)
+  }, [token, submit])
 
   return (
     <>
@@ -13,7 +25,7 @@ function RootLayout() {
         <Outlet />
       </main>
     </>
-  );
+  )
 }
 
-export default RootLayout;
+export default RootLayout
